e80e03d556f31477d9e570aee5c3774cf8dabd65
Дмитрий <mityai20031990@gmail.com>
2022-05-14 23:49:59 +0300

    5 points done

--- app.py
+++ app.py
@@ -1,45 +1,46 @@
 from flask import Flask, render_template, request, jsonify
-from utils import *
+from posts_dao import PostsDao
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+posts_dao = PostsDao()
 
 
 @app.route('/')
 def main_page():
-    posts = get_all_posts()
+    posts = posts_dao.get_all_posts()
     return render_template('index.html', posts=posts)
 
 
 @app.route('/posts/<int:post_pk>/')
 def post_page(post_pk):
-    post = get_post_by_pk(post_pk)
-    comments = get_comments_by_post_id(post_pk)
+    post = posts_dao.get_post_by_pk(post_pk)
+    comments = posts_dao.get_comments_by_post_id(post_pk)
     return render_template('post.html', post=post, comments=comments)
 
 
 @app.route('/search/')
 def search_page():
     search_by = request.args.get('s')
-    posts = search_for_posts(search_by)
+    posts = posts_dao.search_for_posts(search_by)
     return render_template('search.html', posts=posts, search_by=search_by)
 
 
 @app.route('/users/<username>/')
 def user_page(username):
-    post = get_post_by_username(username)
+    post = posts_dao.get_post_by_username(username)
     return render_template('user-feed.html', post=post)
 
 
 @app.route('/api/posts/', methods=['GET'])
 def posts_json():
-    posts = get_all_posts()
+    posts = posts_dao.get_all_posts()
     return jsonify(posts)
 
 
 @app.route('/api/posts/<int:post_id>/', methods=['GET'])
 def post_json(post_id):
-    post = get_post_by_pk(post_id)
+    post = posts_dao.get_post_by_pk(post_id)
     return jsonify(post)
 
 
---
+++ posts_dao.py
@@ -0,0 +1,51 @@
+import json
+
+
+class PostsDao:
+    def get_all_posts(self):
+        with open('data.json', 'r', encoding='utf-8') as f:
+            all_posts = json.load(f)
+            return all_posts
+
+    def get_post_by_username(self, username):
+        all_posts = self.get_all_posts()
+        posts = []
+
+        for item in all_posts:
+            if username.lower() == item['poster_name'].lower():
+                item['comments'] = self.get_comments_by_post_id(item['pk'])
+                item['comments_count'] = len(item['comments'])
+                posts.append(item)
+        return posts
+
+    def get_comments_by_post_id(self, post_id):
+        with open('comments.json', 'r', encoding='utf-8') as f:
+            all_comments = json.load(f)
+            all_comments_by_id = []
+            for item in all_comments:
+                if post_id == item['post_id']:
+                    all_comments_by_id.append(item)
+            return all_comments_by_id
+
+    def search_for_posts(self, key_word):
+        all_posts = self.get_all_posts()
+        posts_lists = []
+        for post in all_posts:
+            if key_word in post['content']:
+                posts_lists.append(post)
+        return posts_lists
+
+    def get_post_by_pk(self, pk):
+        all_posts = self.get_all_posts()
+        posts_list = []
+        for post in all_posts:
+            if pk == post['pk']:
+                post['comments'] = self.get_comments_by_post_id(post['pk'])
+                post['comments_count'] = len(post['comments'])
+                posts_list.append(post)
+        return posts_list
+
+dao=PostsDao()
+print(isinstance(dao.get_comments_by_post_id(2)[0],str))
+print(isinstance(dao.search_for_posts("ага"), list))
+print(dao.search_for_posts("кот"))
